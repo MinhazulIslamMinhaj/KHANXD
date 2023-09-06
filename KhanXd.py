76bf5e1f6fee240d7714f792bcd2de8ab4642822
MinhazulIslamMinhaj <143911340+MinhazulIslamMinhaj@users.noreply.github.com>
2023-09-06 12:54:41 +0600

    Add files via upload

--- KhanXd.py
+++ KhanXd.py
@@ -142,7 +142,9 @@ def Public():
          elif pil in ['2','02']: 
                  try: 
                          cookie=input(" [+] Cookie : ") 
-                         data = requests.get("https://business.facebook.com/business_locations", headers = {"user-agent": "Mozilla/5.0 (Linux; Android 12.1.0; MI 8 Build/OPM1.171019.011) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.86 Mobile Safari/537.36","referer": "https://www.facebook.com/","host": "business.facebook.com","origin": "https://business.facebook.com","upgrade-insecure-requests" : "1","accept-language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7","cache-control": "max-age=0","accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8","content-type":"text/html; charset=utf-8"}, cookies = {"cookie":cookie})  
+                         data = requests.get("https://business.facebook.com/business_locations", headers = {"user-agent": 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36',
+    'viewport-width': '980',}response = requests.get('https://x.facebook.com/', cookies=cookies, headers=headers) 
+
                          find_token = re.search("(EAAG\w+)", data.text) 
                          ken=open(".token.txt", "w").write(find_token.group(1)) 
                          print (" [] Login Successful") 
